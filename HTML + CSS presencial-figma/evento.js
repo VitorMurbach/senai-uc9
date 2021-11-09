560b8bf2c6fc5a1e566820c33a3a52c1cde2ecab
Adriano Santos <adrian.sbueno@gmail.com>
2021-11-08 22:40:27 -0300

    Jquery e "sobre nós" adcionados

--- HTML + CSS presencial-figma/evento.js
+++ HTML + CSS presencial-figma/evento.js
@@ -1,9 +1,16 @@
 document.querySelector("#menu-mobile")
-.addEventListener("click",()=>{
-  var elemento = document.querySelector("#menu-slide")
-  if (elemento.classList.contains("slide-menu-show")){
-    elemento.classList.remove("slide-menu-show")
-  } else {
-    elemento.classList.add("slide-menu-show")
-  }
-})
+  .addEventListener("click", () => {
+    var elemento = document.querySelector("#menu-slide")
+    if (elemento.classList.contains("slide-menu-show")) {
+      elemento.classList.remove("slide-menu-show")
+    } else {
+      elemento.classList.add("slide-menu-show")
+    }
+  })
+function principal1() {
+  window.location.href = "principal.html";
+}
+
+$('#toggle-fading').click(function(){
+  $('.loja-img').fadeToggle();
+});
